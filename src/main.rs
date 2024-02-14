f69ba5303c7ba0d6428c439ef24b72d0d2188ea3
MarkNovik <novikmark@gmail.com>
2024-02-14 14:07:42 +0200

    Added comments support and boolean literals

--- src/main.rs
+++ src/main.rs
@@ -5,18 +5,19 @@ use bigdecimal::num_bigint::BigInt;
 use nom::{AsChar, IResult, Parser};
 use nom::branch::alt;
 use nom::bytes::complete::{tag, tag_no_case};
-use nom::character::complete::{char, hex_digit1, multispace0, multispace1};
+use nom::character::complete::{char as single_char, hex_digit1, multispace0, multispace1};
 use nom::combinator::{eof, fail};
 use nom::multi::{many0, many1, separated_list0};
 use nom::number::complete::recognize_float;
 use nom::sequence::{preceded, terminated};
+
 
 use crate::runtime::{Command, Kroth, KrothType, KrothValue};
 
 mod runtime;
 
 fn main() {
-    let input = std::fs::read_to_string("main.kr").unwrap();
+    let input = remove_comments(&std::fs::read_to_string("main.kr").unwrap());
     match terminated(parse_input, eof)(&input) {
         Ok((_, program)) => {
             let mut kroth = Kroth::default();
@@ -28,11 +29,21 @@ fn main() {
     };
 }
 
+fn remove_comments(input: &str) -> String {
+    input.lines().map(|line| {
+        match line.find("//") {
+            None => line,
+            Some(index) => &line[..index]
+        }.to_string() + "\n"
+    }).collect()
+}
+
 fn parse_input(input: &str) -> IResult<&str, Vec<Command>> {
     separated_list0(multispace1, alt((
         string_literal.map(|s| Command::Push(KrothValue::String(s))),
         number_literal.map(|n| Command::Push(KrothValue::Number(n))),
         type_literal.map(|t| Command::Push(KrothValue::Type(t))),
+        bool_literal.map(|b| Command::Push(KrothValue::Bool(b))),
         basic_command,
         block.map(|cmds| Command::Push(KrothValue::Block(cmds))),
         var
@@ -40,7 +51,7 @@ fn parse_input(input: &str) -> IResult<&str, Vec<Command>> {
 }
 
 fn block(input: &str) -> IResult<&str, Vec<Command>> {
-    terminated(preceded(terminated(char('{'), multispace0), parse_input), preceded(multispace0, char('}')))(input.trim())
+    terminated(preceded(terminated(single_char('{'), multispace0), parse_input), preceded(multispace0, single_char('}')))(input.trim())
 }
 
 fn basic_command(input: &str) -> IResult<&str, Command> {
@@ -57,13 +68,13 @@ fn basic_command(input: &str) -> IResult<&str, Command> {
     ))(input)
 }
 
-
 fn type_literal(input: &str) -> IResult<&str, KrothType> {
-    terminated(preceded(char('<'), alt((
+    terminated(preceded(single_char('<'), alt((
         tag("Number").map(|_| KrothType::Num),
         tag("String").map(|_| KrothType::Str),
         tag("Type").map(|_| KrothType::Typ),
-    ))), char('>'))(input)
+        tag("Bool").map(|_| KrothType::Bool),
+    ))), single_char('>'))(input)
 }
 
 fn string_char(input: &str) -> IResult<&str, char> {
@@ -89,8 +100,8 @@ fn string_char(input: &str) -> IResult<&str, char> {
 
 fn var(input: &str) -> IResult<&str, Command> {
     alt((
-        preceded(char('&'), var_name).map(Command::SetVar),
-        preceded(char('*'), var_name).map(Command::GetVar)
+        preceded(single_char('&'), var_name).map(Command::SetVar),
+        preceded(single_char('*'), var_name).map(Command::GetVar)
     ))(input)
 }
 
@@ -109,8 +120,15 @@ fn var_name_char(input: &str) -> IResult<&str, char> {
 
 fn string_literal(input: &str) -> IResult<&str, Box<str>> {
     terminated(preceded(
-        char('"'), many0(string_char).map(|v| Box::from(v.iter().collect::<String>()))), char('"'),
+        single_char('"'), many0(string_char).map(|v| Box::from(v.iter().collect::<String>()))), single_char('"'),
     )(input)
+}
+
+fn bool_literal(input: &str) -> IResult<&str, bool> {
+    alt((
+        tag("true").map(|_| true),
+        tag("false").map(|_| false),
+    ))(input)
 }
 
 fn number_literal(input: &str) -> IResult<&str, BigDecimal> {
@@ -119,7 +137,7 @@ fn number_literal(input: &str) -> IResult<&str, BigDecimal> {
     }
 
     alt((
-        preceded(char('0'), preceded(char('x'), hex_number)),
+        preceded(single_char('0'), preceded(single_char('x'), hex_number)),
         recognize_float.map(|s| BigDecimal::from_str(s).unwrap())
     ))(input)
 }
